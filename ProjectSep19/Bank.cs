5292063590c697c9234cfa206def1aff922df85e
ArtKoshh <artur.koshmin@hotmail.com>
2024-08-24 20:15:50 +0200

    Add project files.

---
+++ ProjectSep19/Bank.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSep19
+{
+    /*
+  * Implement a class representing a bank account.
+  * A bank account should have the following features:
+  * - Deposit the specified amount of money
+  * - Withdraw the specified amount of money.
+  *   When the amount of money to widthrdaw is greater than the
+  *   saldo the operation must fail.
+  * - Transfer the specified amount to another account
+  * - Query the Saldo of the account
+  * - Query the Name of the owner
+ */
+
+
+
+
+    internal class Bank
+    {
+       public int Saldo { get; private set; }
+        public string Name { get; private set; }
+
+        public Bank(int initialSaldo, string owner)
+        {
+            Saldo = initialSaldo;
+            Name = owner;
+        }
+
+        public void Withdraw(int w)
+        {
+            if (w > Saldo)
+                throw new Exception("Not enough Credit");
+
+            Saldo -= w;
+
+        }
+
+        public void Deposit(int d)
+        {
+             Saldo += d;
+        }
+
+        public void Transfer(int s, Bank konto)
+        {
+            Withdraw(s);
+            konto.Deposit(s);
+        }
+        
+        
+    }
+
+}
---
+++ ProjectSep19/Program.cs
@@ -0,0 +1,9 @@
+﻿using ProjectSep19;
+
+var acc = new Bank(100,"Artur");
+var acc2 = new Bank(75, "Arr");
+
+Console.WriteLine(acc.Saldo);
+
+acc.Transfer(50, acc2);
+Console.WriteLine($"konto 1: {acc.Saldo} , konto 2: {acc2.Saldo}");
